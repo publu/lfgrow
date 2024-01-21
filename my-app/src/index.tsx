35d112453f2d12c2438e99788c29f5a1d8c04ae8
Pablo <pablo@hashingsystems.com>
2024-01-21 00:07:47 -0600

    subpar themeing architecture

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -21,7 +21,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <WagmiConfig client={client}>
-      <ConnectKitProvider theme="retro">
+      <ConnectKitProvider theme="midnight">
         <App />
       </ConnectKitProvider>
     </WagmiConfig>
